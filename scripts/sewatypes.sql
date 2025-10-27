304912c0985479f6b41646b14c2c4156b0a6f35f
Rahul Sharma <sharmarahul.ece@gmail.com>
2025-10-27 15:14:35 +0530

    script files

---
+++ scripts/sewatypes.sql
@@ -0,0 +1,18 @@
+SET IDENTITY_INSERT SewaTypes ON;
+
+INSERT INTO SewaTypes (Id, SewaName) VALUES (1, 'SHK (5:00 AM to 8:30 AM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (2, 'SHK (7:00 AM to 1:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (3, 'HRT (5:00 AM to 8:30 AM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (4, 'HRT (7:00 AM to 1:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (5, 'SEC - 24x7 (6:00 AM to 6:00 AM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (6, 'LGR (8:00 AM to 3:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (7, 'CTN (8:00 AM to 3:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (8, 'CTN - COOKING (8:00 AM to 3:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (9, 'WTR (7:00 AM to 1:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (10, 'PROJECTS (7:00 AM to 1:00 PM)');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (11, 'FOR BHATI - BICHHAI / KANAT REPAIR');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (12, 'PREVIOUS DAY SECURITY');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (13, 'SEWA @ OWN LAND CENTRES');
+INSERT INTO SewaTypes (Id, SewaName) VALUES (14, 'SEC - EVE (6:00 PM TO 6:00 AM)');
+
+SET IDENTITY_INSERT SewaTypes OFF;
