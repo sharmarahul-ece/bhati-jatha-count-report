dee3040e699e2ad32f978683d26a5fe403b48105
Rahul Sharma <sharmarahul.ece@gmail.com>
2025-11-07 16:08:47 +0530

    Add SQL scripts for AllotedCounts data for Friday and Wednesday

---
+++ scripts/alloted_combined_exported.sql
@@ -0,0 +1,307 @@
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (0, 2, 1, 10),
+  (0, 23, 1, 10),
+  (0, 34, 1, 5),
+  (0, 3, 2, 15),
+  (0, 10, 2, 20),
+  (0, 17, 2, 10),
+  (0, 26, 2, 5),
+  (0, 30, 2, 10),
+  (0, 34, 2, 5),
+  (0, 1, 3, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (0, 16, 3, 10),
+  (0, 28, 3, 10),
+  (0, 29, 3, 40),
+  (0, 36, 3, 15),
+  (0, 5, 4, 10),
+  (0, 8, 4, 5),
+  (0, 14, 4, 10),
+  (0, 15, 4, 10),
+  (0, 22, 4, 10),
+  (0, 24, 4, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (0, 25, 4, 10),
+  (0, 26, 4, 10),
+  (0, 27, 4, 10),
+  (0, 1, 5, 5),
+  (0, 5, 5, 5),
+  (0, 32, 5, 5),
+  (0, 34, 5, 5),
+  (0, 7, 6, 10),
+  (0, 8, 6, 10),
+  (0, 13, 6, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (0, 16, 6, 20),
+  (0, 1, 7, 10),
+  (0, 2, 7, 15),
+  (0, 10, 7, 5),
+  (0, 13, 7, 5),
+  (0, 12, 9, 5),
+  (0, 21, 9, 5),
+  (0, 2, 10, 30),
+  (0, 9, 10, 10),
+  (0, 17, 10, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (0, 18, 10, 10),
+  (0, 19, 10, 5),
+  (0, 28, 10, 15),
+  (0, 29, 10, 10),
+  (4, 17, 1, 25),
+  (4, 21, 1, 25),
+  (4, 36, 1, 10),
+  (4, 11, 2, 25),
+  (4, 36, 2, 25),
+  (4, 5, 3, 50);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (4, 17, 3, 20),
+  (4, 18, 3, 25),
+  (4, 19, 3, 20),
+  (4, 21, 3, 10),
+  (4, 23, 3, 15),
+  (4, 30, 3, 8),
+  (4, 15, 4, 30),
+  (4, 25, 4, 50),
+  (4, 2, 5, 5),
+  (4, 17, 5, 5);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (4, 18, 5, 5),
+  (4, 20, 5, 5),
+  (4, 16, 6, 45),
+  (4, 16, 7, 30),
+  (4, 16, 9, 10),
+  (4, 6, 10, 25),
+  (4, 26, 10, 55),
+  (6, 9, 1, 20),
+  (6, 12, 1, 10),
+  (6, 23, 1, 20);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (6, 24, 1, 30),
+  (6, 25, 1, 20),
+  (6, 28, 1, 10),
+  (6, 31, 1, 30),
+  (6, 10, 2, 20),
+  (6, 11, 2, 25),
+  (6, 19, 2, 50),
+  (6, 36, 2, 25),
+  (6, 2, 3, 30),
+  (6, 4, 3, 20);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (6, 8, 3, 40),
+  (6, 26, 3, 20),
+  (6, 27, 3, 40),
+  (6, 28, 3, 30),
+  (6, 30, 3, 10),
+  (6, 35, 3, 15),
+  (6, 13, 4, 40),
+  (6, 18, 4, 35),
+  (6, 21, 4, 55),
+  (6, 26, 5, 5);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (6, 28, 5, 5),
+  (6, 29, 5, 5),
+  (6, 30, 5, 5),
+  (6, 1, 6, 30),
+  (6, 15, 6, 30),
+  (6, 1, 7, 30),
+  (6, 10, 9, 5),
+  (6, 11, 9, 5),
+  (6, 21, 9, 10),
+  (6, 4, 10, 25);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (6, 10, 10, 30),
+  (6, 13, 10, 10),
+  (6, 28, 10, 50),
+  (1, 2, 1, 25),
+  (1, 14, 1, 15),
+  (1, 16, 1, 20),
+  (1, 35, 1, 20),
+  (1, 8, 2, 45),
+  (1, 13, 2, 20),
+  (1, 17, 2, 50);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (1, 1, 3, 10),
+  (1, 3, 3, 30),
+  (1, 10, 3, 15),
+  (1, 13, 3, 15),
+  (1, 14, 3, 15),
+  (1, 15, 3, 10),
+  (1, 4, 4, 20),
+  (1, 20, 4, 30),
+  (1, 22, 4, 30),
+  (1, 35, 4, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (1, 1, 5, 6),
+  (1, 2, 5, 5),
+  (1, 8, 5, 5),
+  (1, 27, 5, 5),
+  (1, 9, 6, 40),
+  (1, 31, 7, 25),
+  (1, 14, 9, 5),
+  (1, 22, 9, 10),
+  (1, 36, 9, 10),
+  (1, 13, 10, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (1, 27, 10, 40),
+  (1, 30, 10, 25),
+  (2, 2, 1, 30),
+  (2, 6, 1, 5),
+  (2, 8, 1, 30),
+  (2, 26, 1, 30),
+  (2, 5, 2, 35),
+  (2, 7, 2, 40),
+  (2, 33, 2, 15),
+  (2, 2, 3, 30);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (2, 3, 3, 30),
+  (2, 4, 3, 15),
+  (2, 11, 3, 45),
+  (2, 14, 3, 10),
+  (2, 24, 3, 20),
+  (2, 25, 3, 20),
+  (2, 30, 3, 8),
+  (2, 2, 4, 20),
+  (2, 5, 4, 10),
+  (2, 12, 4, 10);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (2, 18, 4, 35),
+  (2, 33, 4, 40),
+  (2, 9, 5, 5),
+  (2, 10, 5, 5),
+  (2, 12, 5, 5),
+  (2, 14, 5, 5),
+  (2, 16, 5, 5),
+  (2, 1, 6, 35),
+  (2, 25, 6, 10),
+  (2, 34, 6, 20);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (2, 1, 7, 30),
+  (2, 5, 9, 5),
+  (2, 34, 9, 10),
+  (2, 7, 10, 10),
+  (2, 10, 10, 40),
+  (2, 12, 10, 20),
+  (2, 33, 10, 30),
+  (3, 7, 1, 25),
+  (3, 9, 1, 30),
+  (3, 35, 1, 20);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (3, 12, 2, 35),
+  (3, 23, 2, 50),
+  (3, 31, 2, 30),
+  (3, 2, 3, 30),
+  (3, 6, 3, 5),
+  (3, 15, 3, 10),
+  (3, 16, 3, 50),
+  (3, 36, 3, 15),
+  (3, 12, 4, 25),
+  (3, 20, 4, 50);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (3, 30, 4, 60),
+  (3, 31, 4, 25),
+  (3, 2, 5, 5),
+  (3, 11, 5, 5),
+  (3, 13, 5, 5),
+  (3, 15, 5, 5),
+  (3, 33, 5, 5),
+  (3, 2, 6, 50),
+  (3, 2, 7, 50),
+  (3, 10, 9, 5);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (3, 32, 9, 10),
+  (3, 23, 10, 10),
+  (3, 32, 10, 90),
+  (3, 3, 1, 30),
+  (5, 7, 1, 25),
+  (5, 16, 1, 20),
+  (5, 23, 1, 20),
+  (5, 35, 1, 20),
+  (5, 1, 2, 45),
+  (5, 24, 2, 55);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (5, 2, 3, 15),
+  (5, 3, 3, 15),
+  (5, 7, 3, 20),
+  (5, 11, 3, 25),
+  (5, 17, 3, 20),
+  (5, 26, 3, 20),
+  (5, 20, 4, 60),
+  (5, 24, 4, 30),
+  (5, 30, 4, 10),
+  (5, 9, 5, 5);
+
+INSERT INTO
+  public."AllotedCounts" ("WeekDay", "CenterId", "SewaTypeId", "Count")
+VALUES
+  (5, 22, 5, 5),
+  (5, 23, 5, 5),
+  (5, 25, 5, 5),
+  (5, 14, 6, 40),
+  (5, 3, 7, 40),
+  (5, 1, 9, 10),
+  (5, 29, 9, 5),
+  (5, 8, 10, 55),
+  (5, 20, 10, 20),
+  (5, 29, 10, 60);
